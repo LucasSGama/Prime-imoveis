976485f70f3c594a75a6c01c7664c7da536f5b45
Lucas-Da-Gama <lucasantosgama3@gmail.com>
2024-06-07 14:53:06 -0300

    Progresso da Tela de cadastro mobile

--- Prime_Imoveis_Mobile/app/pages/Cadastro/Cadastro.jsx
+++ Prime_Imoveis_Mobile/app/pages/Cadastro/Cadastro.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Image, Text, TextInput, Button, SafeAreaView} from 'react-native';
+import { useState } from 'react';
+import { View, Image, Text, TextInput, Button, SafeAreaView, ScrollView} from 'react-native';
 import styles from "./CadastroCSS"
 import ImagemTopoCadastro from "../../../Image/Mulher_Abrindo_Porta_Prime.png"
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -8,9 +9,67 @@ import { useNavigation } from '@react-navigation/native';
 export default function Cadastro() {
     // Usando o hook de navegação para obter acesso para ir para outra tela
     const navigation = useNavigation(); 
+    const [senha, setSenha] = useState('');
+    const [email, setEmail] = useState('');
+    const [invalidEmail, setInvalidEmail] = useState(false);
+    const [confirmSenha, setConfirmSenha] = useState('');
+    const [invalidConfirmSenha, setInvalidConfirmSenha] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+
+
+   
+    
+    // Verificação do input de email para verificar se algo de errado
+    const handleEmailChange = (text) => {
+        if (text === '') {
+          setEmail('');
+        }  else if (text.startsWith('@')) {
+            setInvalidEmail(true);
+            setEmail('');
+      } else if (text.includes('@gmail.com')) {
+          setEmail(text);
+          setInvalidEmail(false);
+        } else if (text.includes('@')) {
+          setInvalidEmail(true);
+          setEmail(text);
+        } else {
+          setEmail(text + '@gmail.com');
+        }
+      };
+
+    //   Aqui coloca o "@gmail.com" apos digitar qualquer letra
+      const inputStyleEmail = email === ''? styles.CadastroInputSemValor :
+  email.includes('@gmail.com')? styles.CadastroInputComValor :
+  styles.CadastroInputComValorInvalido;
+
+   // Aqui verifica se tem algo digitado na senha
+   const inputStyleSenha = senha ? styles.CadastroInputComValor : styles.CadastroInputSemValor;
+
+    // Verificar se a senha ta igual
+    const handleConfirmSenhaChange = (text) => {
+        setConfirmSenha(text);
+        if (text!== senha) {
+          setInvalidConfirmSenha(true);
+        } else {
+          setInvalidConfirmSenha(false);
+        }
+      };
+      
+      const inputStyleConfirmSenha = confirmSenha === ''? styles.CadastroInputSemValor : invalidConfirmSenha? styles.CadastroInputComValorInvalido : styles.CadastroInputComValor;
+        
+
+      const handleShowPassword = () => {
+        setShowPassword(!showPassword);
+    }
+    const handleShowConfirmPassword = () => {
+        setShowConfirmPassword(!showConfirmPassword);
+      }
 
   return (
-    <SafeAreaView style={styles.containerCadastro}>
+    <SafeAreaView style={styles.ConfigContainerCadastro}>
+    <ScrollView style={styles.containerCadastro}>
         {/* ++ PARTE DE CIMA DO CADASTRO */}
       <View style={styles.cadastroTopo}>
         <View style={styles.cadastroTopoImagem}>
@@ -21,7 +80,7 @@ export default function Cadastro() {
           />
         </View>
         <View style={styles.CadastroTopoTitulo}>
-            <Text>Cadastro</Text>
+            <Text style={styles.CadastroTopoTituloText}>Cadastro</Text>
         </View>
       </View>
         {/* -- PARTE DE CIMA DO CADASTRO */}
@@ -31,40 +90,47 @@ export default function Cadastro() {
             <View style={styles.CadastroMeioInputs}>
 
                 {/* ++ INPUTS EMAIL */}
-                <View style={styles.CadastroLayoutInputsEmail}>
-                    <Text>Email</Text>
+                <View style={styles.CadastroLayoutInputs}>
+                    <Text style={styles.CadastroLayoutInputsText}>Email</Text>
                     <TextInput
-                    style={[styles.Cadastroinput, styles.CadastroInputEmail]}
-                    // value={username}
-                    // onChangeText={setUsername}
-                    placeholder="Insira seu Email"
+                    style={inputStyleEmail}
+                    keyboardType="email-address"
+                    placeholder="@gmail.com"
+                    onChangeText={handleEmailChange}
+                    value={email}
                     />
                 </View>
                 {/* -- INPUTS EMAIL */}
                 
                 {/* ++ INPUTS SENHA */}
-                <View style={styles.CadastroInputsSenha}>
-                    <Text>Senha</Text>
+                <View style={styles.CadastroLayoutInputs}>
+                    <Text style={styles.CadastroLayoutInputsText}>Senha</Text>
                         <TextInput
-                        style={[styles.Cadastroinput, styles.CadastroInputSenha]}
-                        // value={username}
-                        // onChangeText={setUsername}
+                        secureTextEntry={!showPassword}
+                        style={[inputStyleSenha, styles.CadastroInputSenha]}
+                        value={senha}
+                        onChangeText={setSenha}
+                        placeholderTextColor="gray"
                         placeholder="Insira sua Senha"
                     />
+                    <Icon name={showPassword? "visibility" : "visibility-off"} size={24} color="#000" style={{ position: 'absolute', top: 51, right: 15 }} onPress={handleShowPassword} />
                 </View>
                 {/* -- INPUTS EMAIL */}
                 
                 {/* ++ INPUTS CONFIRMA SENHA */}
-                <View style={styles.CadastroInputsConfirmaSenha}>
-                    <Text>Confirme sua senha</Text>
-                        <TextInput
-                        style={[styles.Cadastroinput, styles.CadastroInputConfirmaSenha]}
-                        // value={username}
-                        // onChangeText={setUsername}
+                <View style={styles.CadastroLayoutInputs}>
+                    <Text style={styles.CadastroLayoutInputsText}>Confirme sua senha</Text>
+                    <TextInput
+                        secureTextEntry={!showConfirmPassword}
+                        style={[inputStyleConfirmSenha, styles.CadastroInputConfirmaSenha]}
+                        value={confirmSenha}
+                        onChangeText={handleConfirmSenhaChange}
                         placeholder="Confirme sua senha"
                     />
+                    <Icon name={showConfirmPassword? "visibility" : "visibility-off"} size={24} color="#000" style={{ position: 'absolute', top: 51, right: 15 }} onPress={handleShowConfirmPassword} />
                 </View>
                 {/* -- INPUTS CONFIRMA SENHA */}
+                </View>
 
                 {/* ++ BOTÃO PARA CADASTRAR */}
                 <View style={styles.CadastroMeioBotão}>
@@ -75,7 +141,6 @@ export default function Cadastro() {
                 </View>
                 {/* -- BOTÃO PARA CADASTRAR */}
 
-            </View>
         </View>
         {/* -- PARTE DO MEIO DO CADASTRO */}
 
@@ -91,6 +156,7 @@ export default function Cadastro() {
             </View>
         </View>
         {/* ++ PARTE DE BAIXO DO CADASTRO */}
+        </ScrollView>
     </SafeAreaView>
   );
 }
--- Prime_Imoveis_Mobile/app/pages/Cadastro/CadastroCSS.jsx
+++ Prime_Imoveis_Mobile/app/pages/Cadastro/CadastroCSS.jsx
@@ -1,35 +1,134 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+    ConfigContainerCadastro: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 1, // Largura da borda
+        borderColor: 'black', // Cor da borda
+        marginTop: 40,
+    },
     containerCadastro: {
         display: 'flex',
-        alignItems: 'center',
+        // alignItems: 'center',
+        height: '100%',
+        width: '90%',
         flexDirection: 'column',
         borderWidth: 1, // Largura da borda
         borderColor: 'black', // Cor da borda
-    },
+    }, 
     cadastroTopo: {
-        borderWidth: 1, // Largura da borda
-        borderColor: 'black', // Cor da borda
         display: 'flex',
-        padding: '50px',
+        flexDirection: 'column',
         alignItems: 'center',
-        width: '90%',
-    },
-    cadastroTopoImagem: {
+        padding: '10px',
+        width: '100%',
         borderWidth: 1, // Largura da borda
         borderColor: 'black', // Cor da borda
-        width: '50%',
+        marginBottom: '3%',
+    },
+    cadastroTopoImagem:{
+        borderWidth: 1, // Largura da borda
+        borderColor: 'black', // Cor da borda
+        width: '100%',
+        alignItems: 'center',
     },
     CadastroImage: {
-        borderWidth: 1,
-        borderColor: 'black',
-        // width: '100%',
+        width: 270,
+        height: 270,
+        borderWidth: 1, // Largura da borda
+        borderColor: 'black', // Cor da borda
+        resizeMode: 'contain',
     },
     CadastroTopoTitulo: {
-        borderWidth: 1,
-        borderColor: 'black',
-    }
+        borderWidth: 1, // Largura da borda
+        borderColor: 'black', // Cor da borda
+        width: '100%',
+        alignItems: 'center',
+    },
+    CadastroTopoTituloText: {
+        fontWeight: 'bold',
+        fontSize: 20,
+    },
+    CadastroMeio: {
+        borderWidth: 1, // Largura da borda
+        borderColor: 'black', // Cor da borda
+        width: '100%',
+        alignItems: 'center',
+        paddingTop: 10,
+        paddingBottom: 10,
+    },
+    CadastroMeioInputs: {
+        borderWidth: 1, // Largura da borda
+        borderColor: 'black', // Cor da borda
+        width: '100%',
+        marginBottom: 10,
+        gap: 10,
+    },
+    CadastroLayoutInputs: {
+        borderWidth: 1, // Largura da borda
+        borderColor: 'black', // Cor da borda
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 10,
+        paddingTop: 10,
+        gap: 10,
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    CadastroLayoutInputsText: {
+        borderWidth: 1, // Largura da borda
+        borderColor: 'black', // Cor da borda
+        fontWeight: 'bold',
+        fontSize: 20,
+    },
+    CadastroInputSemValor: {
+        borderWidth: 0,
+        borderTopWidth: 3,
+        borderTopColor: 'black',
+        fontSize: 15,
+        borderTopLeftRadius: 10,
+        borderBottomRightRadius: 10,
+        shadowColor: '#888888', // cor da sombra
+        shadowOffset: { width: 2, height: 2 }, // shadow offset
+        shadowOpacity: 0.25, // shadow opacity
+        shadowRadius: 4, // shadow radius
+        elevation: 2,
+        paddingLeft: 10,
+        transition: 'opacity 0.3s, border-bottom-width 0.3s, border-bottom-color 0.3s',
+
+    },
+    CadastroInputComValor: {
+        borderWidth: 0,
+        borderTopWidth: 3,
+        borderTopColor: 'green',
+        fontSize: 15,
+        borderTopLeftRadius: 10,
+        borderBottomRightRadius: 10,
+        shadowColor: '#888888', // cor da sombra
+        shadowOffset: { width: 2, height: 2 }, // shadow offset
+        shadowOpacity: 0.25, // shadow opacity
+        shadowRadius: 4, // shadow radius
+        elevation: 2,
+        paddingLeft: 10,
+        transition: 'opacity 0.3s, border-bottom-width 0.3s, border-bottom-color 0.3s',
+    },
+    CadastroInputComValorInvalido: {
+        borderWidth: 0,
+        borderTopWidth: 3,
+        borderTopColor: 'red',
+        fontSize: 15,
+        borderTopLeftRadius: 10,
+        borderBottomRightRadius: 10,
+        shadowColor: '#888888', // cor da sombra
+        shadowOffset: { width: 2, height: 2 }, // shadow offset
+        shadowOpacity: 0.25, // shadow opacity
+        shadowRadius: 4, // shadow radius
+        elevation: 2,
+        paddingLeft: 10,
+        transition: 'opacity 0.3s, border-bottom-width 0.3s, border-bottom-color 0.3s',
+    },
 })
 
 export default styles;
